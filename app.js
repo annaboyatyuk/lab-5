65aee51ce3afc79cd88ea9d9d6def5d4f9a8a568
annaboyatyuk <annaboyatyuk@gmail.com>
2018-01-20 13:44:08 -0800

    what is happening

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -7,11 +7,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
+  var varSum= a + b;
+  var array = [varSum, 'The sum of ' + a + ' and ' + b + ' is ' + varSum + '.'];
+  console.log(array);
+  return array;
+} 
   //eslint-disable-line
-}
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
